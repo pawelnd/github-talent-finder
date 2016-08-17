ae4a4e6381f393c89cab7de10f3a475ff70ba17a
pzych <pawel.zych@gmail.com>
2016-08-17 23:36:40 +0200

    Initial commit - angular seed project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { WebpackAsyncRoute } from '@angularclass/webpack-toolkit';
+import { RouterConfig } from '@angular/router';
+import { Home } from './home';
+
+import { DataResolver } from './app.resolver';
+
+export const routes: RouterConfig = [
+  { path: '', component: Home },
+];
