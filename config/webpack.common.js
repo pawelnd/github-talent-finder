77e892858acf61698003f523cfaa3eae593ebc8a
pzych <pawel.zych@gmail.com>
2016-08-23 01:06:43 +0200

    Changed title

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -18,7 +18,7 @@ const HtmlElementsPlugin = require('./html-elements-plugin');
  * Webpack Constants
  */
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'GitHub Talent Finder',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
